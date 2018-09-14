2017972c8a4518d0ec7b15bf07147836947daf11
vietnguyenp <viet.nguyen@asiantech.vn>
2018-09-15 02:16:04 +0700

    remove unuse code

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/internal/operators';
-import { JwtHelper } from 'angular2-jwt';
 import { environment } from '../../../environments/environment';
 
 const CLIENT_ID = 'fooClientIdPassword';
@@ -11,8 +10,6 @@ const CLIENT_ID = 'fooClientIdPassword';
   providedIn: 'root'
 })
 export class AuthService {
-
-  jwtHelper: JwtHelper = new JwtHelper();
 
   constructor(
     private http: HttpClient
