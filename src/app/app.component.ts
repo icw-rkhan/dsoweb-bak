6e592d309093c06e590683cd6fc21833709c5bbe
Alexander Ospina <ingaxl@gmail.com>
2018-09-14 13:31:09 -0500

    Hide main menu in auth route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'dso-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  withMainMenu: boolean;
+
+  constructor(private router: Router) {
+    this.withMainMenu = false;
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(router => {
+      if (router instanceof  NavigationStart) {
+        const math = router.url.match(/^(?!.*auth).*$/g);
+        this.withMainMenu = math !== null;
+      }
+    });
+  }
 
 }
