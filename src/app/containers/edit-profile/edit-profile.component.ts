c9364bce566c3d4098a57ba3a996d3b80b1dea15
vietnguyenp <viet.nguyen@asiantech.vn>
2018-09-15 23:14:21 +0700

    commit checkout

--- src/app/containers/edit-profile/edit-profile.component.ts
+++ src/app/containers/edit-profile/edit-profile.component.ts
@@ -12,24 +12,51 @@ export class EditProfileComponent implements OnInit {
   is_student: number;
   userInfo: any;
   userProfile: any;
+  metadata: any;
 
   constructor(
     private authService: AuthService,
     private profileService: ProfileService
   ) {
+    this.metadata = {
+      dentalSchool: [],
+      residency: [],
+      practiceRole: [],
+      practiceType: []
+    };
     this.userInfo = this.authService.getUserInfo();
   }
 
   ngOnInit() {
     this.is_student = +localStorage.getItem('is_student');
+    this.getMetaData();
     this.fetchProfile(this.userInfo.user_name);
+  }
+
+  getMetaData() {
+    this.profileService.getMetaData().subscribe(
+      (data: any) => {
+        console.log(data);
+        if (data[0]) {
+          this.metadata.residency = data[0].resultMap.data;
+        }
+        if (data[1]) {
+          this.metadata.dentalSchool = data[1].resultMap.data;
+        }
+        if (data[2]) {
+          this.metadata.practiceRole = data[2].resultMap.data;
+        }
+        if (data[3]) {
+          this.metadata.practiceType = data[3].resultMap.data;
+        }
+      }
+    );
   }
 
   fetchProfile(email: string) {
     this.profileService.findOneByEmail({ email: email }).subscribe(
       (data: any) => {
         this.userProfile = data.resultMap.data;
-        console.log(this.userProfile);
         this.parseData();
       }
     );
