7e1113f732a3381399bd6bf980fe4e67596a544a
Alexander Ospina <ingaxl@gmail.com>
2018-09-18 13:09:29 -0500

    * Fixed top bar with scroll * Code refactoring, removed feed pages and created a generic post type page.

--- src/app/containers/feed/latest-page/latest-page.ts
+++ src/app/containers/feed/latest-page/latest-page.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { forkJoin, Observable, Subscription } from 'rxjs';
+import { forkJoin, Subscription } from 'rxjs';
 import { MatSnackBar } from '@angular/material';
 import { NgProgress } from '@ngx-progressbar/core';
 import * as _ from 'lodash';
@@ -17,7 +17,7 @@ import { AuthService } from '../../../services';
 })
 export class LatestPageComponent implements OnInit, OnDestroy {
 
-  posts$: Observable<Post[]>;
+  posts: Post[];
 
   private postSub: Subscription;
 
@@ -28,7 +28,9 @@ export class LatestPageComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.progress.start();
     const email = this.authService.getUserInfo().user_name;
-    this.posts$ = forkJoin(
+
+    // Join bookmarks and feed
+    this.postSub = forkJoin(
       this.postService.posts(),
       this.bookmarkService.getAllByEmail(email)
     ).pipe(
@@ -39,9 +41,10 @@ export class LatestPageComponent implements OnInit, OnDestroy {
           bookmarkId: !_.isUndefined(bookmark) ? bookmark.id : undefined
         });
       }))
-    );
-
-    this.postSub = this.posts$.subscribe(() => this.progress.complete());
+    ).subscribe(posts => {
+      this.posts = posts;
+      this.progress.complete();
+    });
   }
 
   ngOnDestroy(): void {
