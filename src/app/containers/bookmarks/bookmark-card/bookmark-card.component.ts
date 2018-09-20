df6da057d4e5f52f94d053deca8ceeeb20cc5a49
unknown <dslobodan304@gmail.com>
2018-09-20 22:26:49 +0800

    post the bookmark when user clicks the wherever of the bookmark

--- src/app/containers/bookmarks/bookmark-card/bookmark-card.component.ts
+++ src/app/containers/bookmarks/bookmark-card/bookmark-card.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { Post } from '../../../models/post.model';
 
 @Component({
@@ -12,8 +13,19 @@ export class BookmarkCardComponent {
   @Input() post: Post;
   @Output() remove = new EventEmitter<Post>();
 
+  constructor(private router: Router) {
+
+  }
+
   onRemove() {
     this.remove.emit(this.post);
+  }
+  // post a bookmark by url
+  onPostBookmark(content) {
+    const start = content.indexOf('href=\"');
+    const end = content.indexOf('/\"');
+    const url =  content.substring(start + 6, end);
+    location.href = url;
   }
 
 }
