f39aff9443358ea0cfd905d927516d7783e1cb23
Alexander Ospina <ingaxl@gmail.com>
2018-09-21 12:31:47 -0500

    Merge remote-tracking branch 'remotes/origin/master' into DSD-linkedin
    # Conflicts:
    #	src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgProgressModule } from '@ngx-progressbar/core';
+import { MatDatepickerModule } from '@angular/material';
+import { LinkedInSdkModule } from 'angular-linkedin-sdk';
 
-import {SharedModule} from './shared';
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app.routing';
-import {ReviewsModule} from './containers/reviews/reviews.module';
-import {FeedModule} from './containers/feed/feed.module';
-import {DetailModule} from './containers/detail/detail.module';
-import {SearchPageModule} from './containers/search/search-page.module';
-import {CategoryPageModule} from './containers/category/category-page.module';
-import {BookmarksPageModule} from './containers/bookmarks/bookmarks-page.module';
-import {ProfileModule} from './containers/profile/profile.module';
-import {EditProfileModule} from './containers/edit-profile/edit-profile.module';
-import {AuthGuard} from './services/auth/auth-guard';
-import {MatDatepickerModule} from '@angular/material';
+import { SharedModule } from './shared';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { ReviewsModule } from './containers/reviews/reviews.module';
+import { FeedModule } from './containers/feed/feed.module';
+import { DetailModule } from './containers/detail/detail.module';
+import { SearchPageModule } from './containers/search/search-page.module';
+import { CategoryPageModule } from './containers/category/category-page.module';
+import { BookmarksPageModule } from './containers/bookmarks/bookmarks-page.module';
+import { ProfileModule } from './containers/profile/profile.module';
+import { EditProfileModule } from './containers/edit-profile/edit-profile.module';
+import { AuthGuard } from './services/auth/auth-guard';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
