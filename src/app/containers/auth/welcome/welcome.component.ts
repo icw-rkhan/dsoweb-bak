d4d3e8bdb7e3d8b76d402dc56dc936b1501ef539
unknown <dslobodan304@gmail.com>
2018-09-28 00:22:30 +0800

    only go to student page when click sign up

--- src/app/containers/auth/welcome/welcome.component.ts
+++ src/app/containers/auth/welcome/welcome.component.ts
@@ -24,6 +24,10 @@ export class WelcomeComponent implements OnInit {
 
   signUpOrLogin(signup: boolean = false) {
     this.signup = signup;
+
+    if (signup === false) {
+      this.router.navigate(['/auth', 'login']);
+    }
     this.checkIsStudent = true;
   }
 
