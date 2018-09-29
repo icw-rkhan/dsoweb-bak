d80c2858a828fbe45c43d3186c81174e781ba014
unknown <dslobodan304@gmail.com>
2018-09-29 19:53:01 +0800

    only display categories not prefixed with an “*” on category page

--- src/app/containers/category/category-page.ts
+++ src/app/containers/category/category-page.ts
@@ -19,7 +19,7 @@ import { AuthService } from '../../services';
 })
 export class CategoryPageComponent implements OnInit {
 
-  categories$: Observable<Category[]>;
+  categories$: Category[];
   posts: Post[];
   sponsorId: number;
 
@@ -33,7 +33,18 @@ export class CategoryPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.categories$ = this.categoryService.categories;
+    this.categoryService.categories.subscribe(date => {
+      const categoryList = [];
+      let i = 0;
+      for (i = 0; i < date.length; i++) {
+        const categoryItem = date[i];
+        if (!categoryItem.name.includes('*')) {
+          categoryList.push(categoryItem);
+        }
+      }
+
+      this.categories$ = categoryList;
+    });
   }
 
   selectCategory(event: MatSelectChange) {
@@ -91,7 +102,6 @@ export class CategoryPageComponent implements OnInit {
 
     // Services
     const email = this.authService.getUserInfo().user_name;
-    
     let postService = this.postService.posts({
       page,
       per_page: 5
