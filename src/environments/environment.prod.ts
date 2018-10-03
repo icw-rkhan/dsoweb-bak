57f622a3cfa8e08ecfaf2bbae2b425b3e634286d
unknown <dslobodan304@gmail.com>
2018-10-03 17:10:59 +0200

    update API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   cmsApiUrl: 'https://wp.dsodentist.com/wp-json/wp/v2',
-  profileApiUrl: 'https://upapi.dsodentist.com/profile/profileservice/v1',
+  profileApiUrl: 'https://devupapi1.dsodentist.com/profile/profileservice/v1',
   linkedinClientId: '',
   linkedingClientSecret: ''
 };
