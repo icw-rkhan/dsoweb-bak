e32ed2d5a3427c3b4620cea743f3d68280b291c2
BearandYoon <luckysevenbear@gmail.com>
2018-11-20 16:34:16 +0100

    - implemented DSD-482

--- src/app/containers/unite/unite-search/unite-search.component.ts
+++ src/app/containers/unite/unite-search/unite-search.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { NgProgress } from '@ngx-progressbar/core';
 
 import { PostService } from '../../../services/post.service';
 import { Post } from '../../../models/post.model';
+
 
 @Component({
   selector: 'dso-unite-search',
   templateUrl: './unite-search.component.html',
   styleUrls: ['./unite-search.component.scss']
 })
-export class UniteSearchComponent implements OnInit {
+export class UniteSearchComponent implements OnInit, OnDestroy {
 
   term: string;
   issueId: string;
-
   posts: Post[];
+  searchTyping$: Subject<any> = new Subject<any>();
+  searchTypingSub: Subscription = new Subscription();
 
   constructor(
     private router: Router,
@@ -24,22 +28,27 @@ export class UniteSearchComponent implements OnInit {
     private postService: PostService) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.issueId = params['id'];
+    });
+
+    this.searchTypingSub = this.searchTyping$.pipe(
+      debounceTime(300)
+    ).subscribe(res => {
+      this.onSearch(res);
+    });
   }
 
-  onSearch() {
+  onSearch(searchValue) {
     this.progress.start();
     this.posts = [];
 
     const body = {
-      'searchValue': this.term,
+      'searchValue': searchValue,
       'united': true,
       'skip': 0,
       'limit': 10
     };
-
-    this.route.params.subscribe(params => {
-      this.issueId = params['id'];
-    });
 
     this.postService.search(body).subscribe(posts => {
       this.posts = posts;
@@ -56,4 +65,7 @@ export class UniteSearchComponent implements OnInit {
     }
   }
 
+  ngOnDestroy() {
+    this.searchTypingSub.unsubscribe();
+  }
 }
