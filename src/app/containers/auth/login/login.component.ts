033bf42721ac59c008f7de043d56373d3ce55e98
0321 <a0321-2@0321s-iMac.local>
2018-11-29 15:58:53 +0100

    fixed the login using Linkedin issue

--- src/app/containers/auth/login/login.component.ts
+++ src/app/containers/auth/login/login.component.ts
@@ -108,8 +108,8 @@ export class LoginComponent implements OnInit {
 
   onLoginLinkedIn() {
     const redirectUri = `${document.location.origin}/auth/login`;
-    const url = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=
-    ${environment.linkedinClientId}&redirect_uri=${redirectUri}&state=1234567`;
+    const url = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=` +
+    `${environment.linkedinClientId}&redirect_uri=${redirectUri}&state=1234567`;
     window.location.href = url;
   }
 
