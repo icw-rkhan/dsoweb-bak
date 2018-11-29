7cfd523cc0e366ddddd8f8bc37258623a959a206
0321 <a0321-2@0321s-iMac.local>
2018-11-29 15:43:18 +0100

    add a client_id variable to the product mode

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,6 @@ export const environment = {
   cmsAPIUrl: 'https://democmsapi1.dsodentist.com/content/contentservice/v1',
   profileApiUrl: 'https://devupapi1.dsodentist.com/profile/profileservice/v1',
   linkedinClientId: '77xn8gu9wv8bfj',
-  linkedingClientSecret: 'SEwGEC8wA7KFuX2i',
   SPONSOR_GSK: '197',
   SPONSOR_ALIGN: '260',
   SPONSOR_NOBEL: '502',
