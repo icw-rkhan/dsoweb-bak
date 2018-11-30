0a9fb2a15f0f4643d05568dca87aa0730dd1f102
0321 <a0321-2@0321s-iMac.local>
2018-11-30 01:46:52 +0100

    modify the env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  cmsAPIUrl: 'https://democmsapi1.dsodentist.com/content/contentservice/v1',
+  cmsAPIUrl: 'https://devcmsapi1.dsodentist.com/content/contentservice/v1',
   profileApiUrl: 'https://devupapi1.dsodentist.com/profile/profileservice/v1',
   linkedinClientId: '77xn8gu9wv8bfj',
   SPONSOR_GSK: '197',
