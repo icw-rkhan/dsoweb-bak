9a4d7b17a30398475e9afea094f154a90c45c883
0321 <a0321-2@0321s-iMac.local>
2018-12-19 13:45:04 +0100

    implement the search page for Career module

--- src/app/containers/career/career-search/career-search.component.ts
+++ src/app/containers/career/career-search/career-search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef,
+  ViewChild, ElementRef, NgZone, AfterViewInit, OnDestroy } from '@angular/core';
+  import { MapsAPILoader } from '@agm/core';
 import { NgProgress } from '@ngx-progressbar/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -11,7 +13,9 @@ import { JobService } from '../../../services/job.service';
   styleUrls: ['./career-search.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CareerSearchComponent implements OnInit, OnDestroy {
+export class CareerSearchComponent implements OnInit, OnDestroy, AfterViewInit {
+
+  @ViewChild('location2') locationElementRef: ElementRef;
 
   term: string;
   type: string;
@@ -20,15 +24,26 @@ export class CareerSearchComponent implements OnInit, OnDestroy {
   location: string;
   showGotoTopBtn: boolean;
 
+  public latitude: number;
+  public longitude: number;
+  public zoom: number;
+
   jobs: Job[];
 
   constructor(
+    private ngZone: NgZone,
     private progress: NgProgress,
     private route: ActivatedRoute,
     private cdr: ChangeDetectorRef,
-    private jobService: JobService) {
+    private jobService: JobService,
+    private mapsAPILoader: MapsAPILoader) {
       this.page = 0;
       this.showGotoTopBtn = false;
+
+      // set google maps defaults
+      this.zoom = 4;
+      this.latitude = 39.8282;
+      this.longitude = -98.5795;
 
       this.route.params.subscribe(params => {
         this.type = params['type'];
@@ -45,12 +60,40 @@ export class CareerSearchComponent implements OnInit, OnDestroy {
     this.progress.complete();
   }
 
+  ngAfterViewInit() {
+    if (this.locationElementRef) {
+      // load Places Autocomplete
+      this.mapsAPILoader.load().then(() => {
+        const autocomplete = new google.maps.places.Autocomplete(this.locationElementRef.nativeElement, {
+          types: ['address']
+        });
+        autocomplete.addListener('place_changed', () => {
+          this.ngZone.run(() => {
+            // get the place result
+            const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+            // verify result
+            if (place.geometry === undefined || place.geometry === null) {
+              return;
+            }
+
+            // set latitude, longitude and zoom
+            this.location = place.name;
+            this.latitude = place.geometry.location.lat();
+            this.longitude = place.geometry.location.lng();
+            this.zoom = 12;
+          });
+        });
+      });
+    }
+  }
+
   onSearch() {
     this.progress.start();
 
     const body = {
       'searchValue': this.term ? this.term : null,
-      'location': this.location ? this.location : null,
+      'location': this.location ? [this.latitude, this.longitude] : null,
       'distance': this.distance ? this.distance : null,
       'skip': this.page * 10,
       'limit': 10
