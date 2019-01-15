b184bf3dbd5d5dde6929907319d83ab57aed2e0c
0321 <a0321-2@0321s-iMac.local>
2019-01-15 16:36:47 +0100

    modify the env for deploying the app

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   careerAPIUrl: 'https://demohrapi1.dsodentist.com/hr/hrservice/v1',
   cmsAPIUrl: 'https://devcmsapi1.dsodentist.com/content/contentservice/v1',
   profileApiUrl: 'https://devupapi1.dsodentist.com/profile/profileservice/v1',
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   careerAPIUrl: 'https://demohrapi1.dsodentist.com/hr/hrservice/v1',
-  cmsAPIUrl: 'https://devcmsapi1.dsodentist.com/content/contentservice/v1',
-  profileApiUrl: 'https://devupapi1.dsodentist.com/profile/profileservice/v1',
+  cmsAPIUrl: 'https://cmsapi1.dsodentist.com/content/contentservice/v1',
+  profileApiUrl: 'https://upapi1.dsodentist.com/profile/profileservice/v1',
   linkedinClientId: '77xn8gu9wv8bfj',
   SPONSOR_GSK: '197',
   SPONSOR_ALIGN: '260',
